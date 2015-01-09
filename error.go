117339f9c201952c3a358873d29cb23a01e22e51
Felipe Cavani <fcavani@gmail.com>
2015-01-09 08:57:45 -0200

    Add the creation date of the file source.

--- error.go
+++ error.go
@@ -1,6 +1,8 @@
 // Copyright 2015 Felipe A. Cavani. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+// Start date:		2013-05-08
+// Last modification:	2013-
 
 // Helper functions to manipulate errors and trace than.
 package e
