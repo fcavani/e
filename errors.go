83ca0b8db0c43ac27d2cd584420c295a45ef22e2
Felipe Cavani <fcavani@gmail.com>
2015-01-12 12:43:06 -0200

    Some commons errors.

---
+++ errors.go
@@ -0,0 +1,12 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Start date:		2015-01-12
+// Last modification:	2015-
+
+package e
+
+const ErrEmptyString = "empty string"
+const ErrInvalidType = "type is invalid"
+const ErrInvalidLength = "length is invalid"
+const ErrInvalidInterface = "invalid interface"
